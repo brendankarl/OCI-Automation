8ddff0d1b7c25938084d1fb82b1af916aba38644
Brendan Griffin <brendankgriffin@hotmail.com>
2025-12-16 12:22:35 +0000

    Update Disable-IMDSv1.ps1

--- Disable-IMDSv1.ps1
+++ Disable-IMDSv1.ps1
@@ -1,3 +1,3 @@
-$InstanceId = "ocid1.instance.oc1.uk-london-1.anwgiljrayvpzvacpmqta5guodp3dfjqpeoncgxczjfckkzgxskkdd5z3dla"
+$InstanceId = "OCID"
 Update-OCIComputeInstance -InstanceId $InstanceId -UpdateInstanceDetails @{InstanceOptions = @{AreLegacyImdsEndpointsDisabled = $true}}
 $Instance = Get-OCIComputeInstance -InstanceId $instanceId
