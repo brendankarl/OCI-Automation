7af76d538cb4e44457af56e0931e93729c38dd8e
Brendan Griffin <brendankgriffin@hotmail.com>
2025-12-16 12:26:20 +0000

    Update Get-All-VMs.ps1

--- Get-All-VMs.ps1
+++ Get-All-VMs.ps1
@@ -1,6 +1,7 @@
 # Get all VMs
 $Compartments = Get-OCIIdentityCompartmentsList -CompartmentId "OCID" -CompartmentIdInSubtree $true -LifecycleState Active
 
+# Loop through each Compartment, identify each VM instance and print the name
 Foreach ($Compartment in $Compartments) 
 {
 Write-Host "Compartment Name:" $Compartment.Name -ForegroundColor Green
