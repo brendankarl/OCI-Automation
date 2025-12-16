727f2cb7133090c1507ec0fe1e12cd1fc5e693d1
Brendan Griffin <brendankgriffin@hotmail.com>
2025-12-16 12:23:04 +0000

    Update Get-All-VMs.ps1

--- Get-All-VMs.ps1
+++ Get-All-VMs.ps1
@@ -1,5 +1,5 @@
 # Get all VMs
-$Compartments = Get-OCIIdentityCompartmentsList -CompartmentId ocid1.tenancy.oc1..aaaaaaaae5qe65hymbveayf2nopsuskv525njloma3k5c4yf5hl75uj75euq -CompartmentIdInSubtree $true -LifecycleState Active
+$Compartments = Get-OCIIdentityCompartmentsList -CompartmentId "OCID" -CompartmentIdInSubtree $true -LifecycleState Active
 
 Foreach ($Compartment in $Compartments) 
 {
