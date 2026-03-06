fc69df1c5c536ff27e000c9827a99fa7a221311e
Brendan Griffin <brendankgriffin@hotmail.com>
2026-03-06 10:24:57 +0000

    Disable legacy IMDS endpoints for all instances
    Added functionality to disable legacy IMDS endpoints for all VMs in specified compartments.

--- Disable-IMDSv1.ps1
+++ Disable-IMDSv1.ps1
@@ -1,3 +1,14 @@
-$InstanceId = "OCID"
-Update-OCIComputeInstance -InstanceId $InstanceId -UpdateInstanceDetails @{InstanceOptions = @{AreLegacyImdsEndpointsDisabled = $true}}
-$Instance = Get-OCIComputeInstance -InstanceId $instanceId
+# Get all Compartments
+$Compartments = Get-OCIIdentityCompartmentsList -CompartmentId TenancyOCID -CompartmentIdInSubtree $true -LifecycleState Active
+
+# Loop through all VMs and update the instance metadata service so that it only supports version 2
+Foreach ($Compartment in $Compartments) 
+{
+Write-Host "Compartment Name:" $Compartment.Name -ForegroundColor Green
+$Instances = Get-OCIComputeInstancesList -CompartmentId $Compartment.Id
+Foreach ($Instance in $Instances)
+    {
+        Write-Host "-Instance:" $Instance.DisplayName -ForegroundColor White
+        $Action = Update-OCIComputeInstance -InstanceId $Instance.Id -UpdateInstanceDetails @{InstanceOptions = @{AreLegacyImdsEndpointsDisabled = $true}}
+    }
+}
